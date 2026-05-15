215e1dab83bc54afea26d1f455e78593280d8a12
Jai Ansh Bindra <jaiansh2005@gmail.com>
2026-05-15 18:02:38 +0200

    I implemented explicit heat equation time step, the same as last commit, I just forgot to push the main.cpp the last time :)

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include "heat2d.hpp"
+
 #include <iostream>
+#include <utility>
 
 int main() {
-    HeatParams p{
-        256,
-        256,
-        0.1,
-        1000,
-        50,
-        "snapshots"
-    };
+    HeatParams p;
+    p.width = 256;
+    p.height = 256;
+    p.lambda = 0.24;
+    p.num_steps = 10;
+    p.snapshot_every = 100;
+    p.out_dir = "snapshots";
 
-    Grid g = make_grid(p);
+    Grid cur = make_grid(p);
+    Grid nxt(p.width * p.height, 0.0);
 
-    std::cout << "Center value initialized.\n";
+    for (int s = 0; s < p.num_steps; ++s) {
+        step_once(cur, nxt, p);
+        std::swap(cur, nxt);
+    }
 
+    std::cout << "Ran " << p.num_steps << " sequential heat steps.\n";
     return 0;
 }
